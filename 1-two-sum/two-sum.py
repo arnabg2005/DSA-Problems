5f6ce91faf50ac0c3e7f215e0574d2af5d83e8f5
Arnab Ghosh <arnabg873@gmail.com>
2026-08-01 21:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (36.17%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap={}
+        for i in range(len(nums)):
+            need=target-nums[i]
+
+            if need in hashmap:
+                return [hashmap[need],i]
+            hashmap[nums[i]]=i
+        
