0afe9019d54f04d68dcf09445ce301b2b5eaad0a
Arnab Ghosh <arnabg873@gmail.com>
2026-08-01 21:10:27 +0530

    Time: 32 ms (26.95%) | Memory: 26.7 MB (13.46%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashmap={}
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                return True
+            hashmap[nums[i]]=i
+        return False
+        
