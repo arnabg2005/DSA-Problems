2ba3f84834e59b82e55202702b5bbf91b78af029
Arnab Ghosh <arnabg873@gmail.com>
2026-08-15 22:59:17 +0530

    Time: 6 ms (23.73%) | Memory: 12.3 MB (92.22%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def myAtoi(self, s):
+        s = s.lstrip()
+        if not s:
+            return 0
+
+        sign = 1
+        i = 0
+        if s[0] == "-":
+            sign = -1
+            i += 1
+        elif s[0] == "+":
+            i += 1
+
+        num = 0
+        while i < len(s) and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+        INT_MIN, INT_MAX = -(2**31), 2**31 - 1
+
+        return max(INT_MIN, min(num, INT_MAX))
