5caf71ec25e5c4b3a03837ec642344d5c5efe041
Arnab Ghosh <arnabg873@gmail.com>
2026-09-08 19:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.39%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -0,0 +1,12 @@
+import math
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total_sum = n * (n + 1) // 2
+        
+        x = int(math.sqrt(total_sum))
+        
+        if x * x == total_sum:
+            return x
+            
+        return -1
